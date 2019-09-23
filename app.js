9476b38cb167bc6430d4c1e081219f6c5d0cb65b
Mikka Pineda <mikka.pineda@38-SLG-Mac.local>
2019-09-23 14:37:27 -0700

    switch region and users

--- app.js
+++ app.js
@@ -1,54 +1,49 @@
-'use strict';
-
+// app.js 
+const sls = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
-const slsHTTP = require('serverless-http') // this module wraps our API for serverless use
-const bodyParser = require('body-parser') // parses the body of HTTP requests
-const AWS = require('aws-sdk') // enables us to interact with AWS services and components
-
-const PLAYERS_TABLE = process.env.PLAYERS_TABLE;
+const AWS = require('aws-sdk');
+const USERS_TABLE = process.env.USERS_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 app.use(bodyParser.json({ strict: false }));
-
-// POST - Create User endpoint
-app.post('/players', function (req, res) {
-  const { playerId, name } = req.body;
-  const params = {
-      TableName: PLAYERS_TABLE,
-      Item: {
-        playerId: playerId,
-        name: name,
-      },
-    };
-  dynamoDb.put(params, (error) => {
-      if (error) {
-        console.log(error);
-        res.status(400).json({ error: `Could not create user ${playerId}` });
-      }
-      res.json({ playerId, name });
-    });
+// Create User endpoint
+app.post('/users', function (req, res) {
+  const { userId, name } = req.body;
+const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
+dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: `Could not create user ${userId}` });
+    }
+    res.json({ userId, name });
+  });
 })
-// GET - Get User endpoint
-app.get('/players/:playerId', function (req, res) {
+// Get User endpoint
+app.get('/users/:userId', function (req, res) {
   const params = {
-    TableName: PLAYERS_TABLE,
+    TableName: USERS_TABLE,
     Key: {
-      playerId: req.params.playerId,
+      userId: req.params.userId,
     },
   }
 dynamoDb.get(params, (error, result) => {
     if (error) {
       console.log(error);
-      res.status(400).json({ error: `Could not get user ${playerId}` });
+      res.status(400).json({ error: `Could not get user ${userId}` });
     }
     if (result.Item) {
-      const {playerId, name} = result.Item;
-      res.json({ playerId, name });
+      const {userId, name} = result.Item;
+      res.json({ userId, name });
     } else {
-      res.status(404).json({ error: `User ${playerId} not found` });
+      res.status(404).json({ error: `User ${userId} not found` });
     }
   });
 })
-
-module.exports.server = slsHTTP(app)
+module.exports.server = sls(app)
